50cd154177ed2b814a214f2666cef827d8efb9d5
FabioCampaner <fabiocsuzuki@gmail.com>
2021-08-23 14:28:12 -0300

    Correções nos exercícios da lista 1

--- springBoot/lista1/exercicio1/exercicio1/src/main/java/com/lista1Exercicio1/EXERCICIO1/controller/Exercicio1Controller.java
+++ springBoot/lista1/exercicio1/exercicio1/src/main/java/com/lista1Exercicio1/EXERCICIO1/controller/Exercicio1Controller.java
@@ -1,12 +1,14 @@
 package com.lista1Exercicio1.EXERCICIO1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/exercicio1")
 public class Exercicio1Controller {
 
-	@RequestMapping("/exercicio1")
+	@GetMapping
 	public String resposta() {
 		return "A mentalidade utilizada para a realização dessa atividade foi a persistência;<br>A habilidade utilizada foi a atenção aos detalhes, por se tratar de um novo conteúdo.";
 	}
--- springBoot/lista1/exercicio2/exercicio2/src/main/java/com/lista1Exercicio2/EXERCICIO2/controller/Exercicio2Controller.java
+++ springBoot/lista1/exercicio2/exercicio2/src/main/java/com/lista1Exercicio2/EXERCICIO2/controller/Exercicio2Controller.java
@@ -1,11 +1,14 @@
 package com.lista1Exercicio2.EXERCICIO2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/exercicio2")
 public class Exercicio2Controller {
-	@RequestMapping("/exercicio2")
+	
+	@GetMapping
 	public String resposta() {
 		return "<b>Objetivos de Aprendizagem</b><br><br>Aprender a utilizar o Spring Boot para fazer a interação back-end com a internet aconteça!";
 	}
