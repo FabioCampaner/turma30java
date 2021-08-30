a4cbcc0ed1a04ffdd45bb16f61c6b9a5ec191998
FabioCampaner <fabiocsuzuki@gmail.com>
2021-08-30 11:31:13 -0300

    Exercício da Minha loja de Games

---
+++ springBoot/minhaLojaDeGames/minhaLojaDeGames/src/main/java/com/minhaLojaDeGames/minhaLojaDeGames/model/Produto.java
@@ -0,0 +1,84 @@
+package com.minhaLojaDeGames.minhaLojaDeGames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name ="produto")
+public class Produto {
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	@Size(max = 50)
+	private String titulo;
+	
+	@NotBlank 
+	@Size(max = 2)
+	private String classificacao;
+	
+	@NotNull
+	private BigDecimal valor;
+	
+	@NotNull
+	private int anoFabricacao;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getClassificacao() {
+		return classificacao;
+	}
+
+	public void setClassificacao(String classificacao) {
+		this.classificacao = classificacao;
+	}
+
+	public int getAnoFabricacao() {
+		return anoFabricacao;
+	}
+
+	public void setAnoFabricacao(int anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
